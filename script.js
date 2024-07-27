c747c829c129edb19f6f79c885016e092781d447
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-27 09:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll('.image');
-    let dragSrcEl = null;
+document.addEventListener('DOMContentLoaded', () => {
+  const draggables = document.querySelectorAll('.image');
 
-    function handleDragStart(e) {
-        dragSrcEl = this;
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', this.innerHTML);
-        this.classList.add('selected');
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+    draggable.addEventListener('dragover', dragOver);
+    draggable.addEventListener('dragenter', dragEnter);
+    draggable.addEventListener('dragleave', dragLeave);
+    draggable.addEventListener('drop', dragDrop);
+    draggable.addEventListener('dragend', dragEnd);
+  });
+
+  let dragSrcEl = null;
+
+  function dragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.outerHTML);
+    this.classList.add('selected');
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function dragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function dragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  function dragDrop(e) {
+    e.preventDefault();
+    if (dragSrcEl !== this) {
+      dragSrcEl.outerHTML = this.outerHTML;
+      this.outerHTML = e.dataTransfer.getData('text/html');
     }
+    return false;
+  }
 
-    function handleDragOver(e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        }
-        e.dataTransfer.dropEffect = 'move';
-        return false;
-    }
-
-    function handleDragEnter(e) {
-        this.classList.add('over');
-    }
-
-    function handleDragLeave(e) {
-        this.classList.remove('over');
-    }
-
-    function handleDrop(e) {
-        if (e.stopPropagation) {
-            e.stopPropagation();
-        }
-
-        if (dragSrcEl !== this) {
-            dragSrcEl.innerHTML = this.innerHTML;
-            this.innerHTML = e.dataTransfer.getData('text/html');
-        }
-        return false;
-    }
-
-    function handleDragEnd(e) {
-        images.forEach((img) => {
-            img.classList.remove('over');
-            img.classList.remove('selected');
-        });
-    }
-
-    images.forEach((img) => {
-        img.addEventListener('dragstart', handleDragStart);
-        img.addEventListener('dragenter', handleDragEnter);
-        img.addEventListener('dragover', handleDragOver);
-        img.addEventListener('dragleave', handleDragLeave);
-        img.addEventListener('drop', handleDrop);
-        img.addEventListener('dragend', handleDragEnd);
+  function dragEnd(e) {
+    draggables.forEach(draggable => {
+      draggable.classList.remove('over');
+      draggable.classList.remove('selected');
     });
+  }
 });
